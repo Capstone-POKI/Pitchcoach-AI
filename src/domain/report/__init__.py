8c3673d1e3692855fa43efc00b815d0ab8229c15
hseo1o2 <0102jhshs@ewha.ac.kr>
2026-02-16 17:32:21 +0900

    refactor(report): expose report module APIs via lazy exports

--- src/domain/report/__init__.py
+++ src/domain/report/__init__.py
@@ -1,3 +1,25 @@
-from src.domain.report.report_builder import build_final_report
+def build_analysis_context(*args, **kwargs):
+    from src.domain.report.feature_builder import build_analysis_context as _build_analysis_context
 
-__all__ = ["build_final_report"]
+    return _build_analysis_context(*args, **kwargs)
+
+
+def generate_qa(*args, **kwargs):
+    from src.domain.report.qa_generator import generate_qa as _generate_qa
+
+    return _generate_qa(*args, **kwargs)
+
+
+def build_final_report(*args, **kwargs):
+    from src.domain.report.report_builder import build_final_report as _build_final_report
+
+    return _build_final_report(*args, **kwargs)
+
+
+def compute_feature_impacts(*args, **kwargs):
+    from src.domain.report.score_engine import compute_feature_impacts as _compute_feature_impacts
+
+    return _compute_feature_impacts(*args, **kwargs)
+
+
+__all__ = ["build_analysis_context", "generate_qa", "build_final_report", "compute_feature_impacts"]
